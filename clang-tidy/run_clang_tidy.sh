9136281cca9b06d5acc9ba5855d0d1a43345fe4f
vukvuk21 <vukvuk21@gmail.com>
2025-03-31 09:24:31 +0200

    Dodate preostale skripte za pokretanje alata

---
+++ clang-tidy/run_clang_tidy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if ! command -v clang-tidy &> /dev/null; then
+    echo "clang-tidy nije instaliran. Instalirati  ga pomoću 'sudo apt install clang-tidy' i pokušati ponovo."
+    exit 1
+fi
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BUILD_DIR="$(realpath "$SCRIPT_DIR/cmake-build-debug")"
+SRC_DIR="$(realpath "$SCRIPT_DIR/..")"
+
+if [ ! -f "$BUILD_DIR/compile_commands.json" ]; then
+    echo "Nije pronađen fajl compile_commands.json u $BUILD_DIR. Uveriti se da je CMake podešen sa -DCMAKE_EXPORT_COMPILE_COMMANDS=ON."
+    exit 1
+fi
+
+echo "Pokrećetanje clang-tidy analize..."
+run-clang-tidy -checks='clang-diagnostic-*,clang-analyzer-*,modernize-use-auto,modernize-use-nullptr,modernize-use-noexcept,modernize-use-emplace,modernize-use-emplace-back,modernize-loop-convert,modernize-use-using' -p "$BUILD_DIR" "$SRC_DIR" > clang-tidy-report.txt 2>&1
+
+if [ -f clang-tidy-report.txt ]; then
+    echo "Analiza je uspešno završena. Izveštaj je sačuvan u fajlu clang-tidy-report.txt"
+else
+    echo "Došlo je do greške tokom analize."
+fi
+
---
+++ cppcheck/run_cppcheck.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Provera da li je Cppcheck instaliran
+if ! command -v cppcheck &> /dev/null; then
+    echo "Cppcheck nije instaliran. Instalirati ga pomoću 'sudo apt install cppcheck' i pokušati ponovo."
+    exit 1
+fi
+
+
+PROJECT_PATH="/home/vuk/Desktop/Zelda-final/zelda/src/"
+OUTPUT_TXT="cpp_check_res.txt"  
+
+
+echo "Pokrećem Cppcheck za projekt: $PROJECT_PATH..."
+cppcheck --enable=all --inconclusive --suppress=missingInclude "$PROJECT_PATH" 2> "$OUTPUT_TXT"
+
+
+if [ ! -f "$OUTPUT_TXT" ]; then
+    echo "Cppcheck nije uspeo da kreira tekstualni fajl. Proverite greške."
+    exit 1
+fi
+
+echo "Izveštaj je uspešno kreiran: $OUTPUT_TXT"
+
+
